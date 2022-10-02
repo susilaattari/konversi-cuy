3e27e92376155c011d82f720b4bb867ce7754912
susila tropika <susilatropika8@gmail.com>
2022-10-02 15:01:02 +0700

    finally

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["index.html"],
   theme: {
     extend: {
-      colors:{
-        "cyian-1":"#ecfeff",
-        "cyian-2":"#cffafe"
-      }
+      colors: {
+        "cyian-1": "#ecfeff",
+        "cyian-2": "#cffafe",
+      },
     },
   },
   plugins: [],
